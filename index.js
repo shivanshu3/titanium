86ed73c68b186be0a623d0ed7d4ce275ef184d18
Shivanshu Goyal <shivanshu3@gmail.com>
2017-12-04 08:38:43 -0800

    Added proper error handling for syntax errors.

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ var parser = require('./grammar.js');
 
 var testInput = fs.readFileSync('./test_input').toString();
 
-var foo = parser.parse(testInput);
-console.log(JSON.stringify(foo, null, '  '));
+function stringifyErrorLocation(location) {
+	function singleLocation (singleLocation) {
+		return 'L:' + singleLocation.line + ' C:' + singleLocation.column;
+	};
+	return singleLocation(location.start) + ' - ' + singleLocation(location.end);
+}
+
+var ast;
+try {
+	ast = parser.parse(testInput);
+} catch (ex) {
+	if (ex.name == 'SyntaxError') {
+		console.error('SyntaxError: ' + ex.message);
+		console.error(stringifyErrorLocation(ex.location));
+	} else {
+		console.error('Unknown Error!');
+		throw ex;
+	}
+}
+
+console.log(JSON.stringify(ast, null, '  '));
 debugger;
