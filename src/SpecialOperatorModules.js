f6edca12ee624f268e502aa4477c49b687a92f00
Shivanshu Goyal <shivanshu3@gmail.com>
2017-12-10 23:29:58 -0800

    special operator modules are in their own module now

---
+++ src/SpecialOperatorModules.js
@@ -0,0 +1,69 @@
+var processExpression;
+
+var modules = {
+	'swap' : function(stack) {
+		var a = stack.pop();
+		var b = stack.pop();
+		stack.push(a);
+		stack.push(b);
+	},
+	'dup' : function(stack) {
+		var a = stack.pop();
+		stack.push(a);
+		stack.push(a);
+	},
+	'if' : function(stack) {
+		var mainBody = stack.pop().expression;
+		var ifExpression = stack.pop().expression;
+		processExpression(ifExpression, stack);
+		var resultIfExpression = stack.pop();
+		if (resultIfExpression) {
+			processExpression(mainBody, stack);
+		}
+	},
+	'ifelse' : function(stack) {
+		var falseBody = stack.pop().expression;
+		var trueBody = stack.pop().expression;
+		var ifExpression = stack.pop().expression;
+		processExpression(ifExpression, stack);
+		var resultIfExpression = stack.pop();
+		if (resultIfExpression) {
+			processExpression(trueBody, stack);
+		} else {
+			processExpression(falseBody, stack);
+		}
+	},
+	'while' : function(stack) {
+		var loopBody = stack.pop().expression;
+		var ifExpression = stack.pop().expression;
+
+		while(processExpression(ifExpression, stack), stack.pop()) {
+			processExpression(loopBody, stack);
+		}
+	},
+	'map' : function(stack) {
+		var procedure = stack.pop();
+		var array = stack.pop();
+		var result = [];
+
+		if (typeof procedure == 'function') {
+			result = array.map(procedure);
+		} else {
+			var titaniumProcedure = procedure.expression;
+			for (var i = 0; i < array.length; i++) {
+				stack.push(array[i]);
+				processExpression(titaniumProcedure, stack);
+				var titaniumProcedureResult = stack.pop();
+				result.push(titaniumProcedureResult);
+			}
+		}
+
+		stack.push(result);
+	}
+};
+
+modules.initialize = function(_processExpression) {
+	processExpression = _processExpression;
+};
+
+module.exports = modules;
--- src/interpreter.js
+++ src/interpreter.js
@@ -1,4 +1,7 @@
 var basicOperatorModules = require('./BasicOperatorModules.js');
+var specialOperatorModules = require('./SpecialOperatorModules.js');
+
+specialOperatorModules.initialize(processExpression);
 
 var theHeap = {};
 var userModules;
@@ -26,69 +29,6 @@ function processExpression(expr, stack) {
 	}
 }
 
-// this is also a hack. should not be a part of the exports object:
-exports.specialOperators = {
-	'swap' : function(stack) {
-		var a = stack.pop();
-		var b = stack.pop();
-		stack.push(a);
-		stack.push(b);
-	},
-	'dup' : function(stack) {
-		var a = stack.pop();
-		stack.push(a);
-		stack.push(a);
-	},
-	'if' : function(stack) {
-		var mainBody = stack.pop().expression;
-		var ifExpression = stack.pop().expression;
-		processExpression(ifExpression, stack);
-		var resultIfExpression = stack.pop();
-		if (resultIfExpression) {
-			processExpression(mainBody, stack);
-		}
-	},
-	'ifelse' : function(stack) {
-		var falseBody = stack.pop().expression;
-		var trueBody = stack.pop().expression;
-		var ifExpression = stack.pop().expression;
-		processExpression(ifExpression, stack);
-		var resultIfExpression = stack.pop();
-		if (resultIfExpression) {
-			processExpression(trueBody, stack);
-		} else {
-			processExpression(falseBody, stack);
-		}
-	},
-	'while' : function(stack) {
-		var loopBody = stack.pop().expression;
-		var ifExpression = stack.pop().expression;
-
-		while(processExpression(ifExpression, stack), stack.pop()) {
-			processExpression(loopBody, stack);
-		}
-	},
-	'map' : function(stack) {
-		var procedure = stack.pop();
-		var array = stack.pop();
-		var result = [];
-
-		if (typeof procedure == 'function') {
-			result = array.map(procedure);
-		} else {
-			var titaniumProcedure = procedure.expression;
-			for (var i = 0; i < array.length; i++) {
-				stack.push(array[i]);
-				processExpression(titaniumProcedure, stack);
-				var titaniumProcedureResult = stack.pop();
-				result.push(titaniumProcedureResult);
-			}
-		}
-
-		stack.push(result);
-	}
-};
-
 function operate(stack, operatorTerm) {
 	// console.log(operatorTerm.opType + ' operating...');
 	if (operatorTerm.opType == 'variableOperator') {
@@ -110,8 +50,8 @@ function operate(stack, operatorTerm) {
 	} else if (userModules[operatorTerm.name] != undefined) {
 		var operatorModule = userModules[operatorTerm.name];
 		applyOperatorModule(stack, operatorModule);
-	} else if (exports.specialOperators[operatorTerm.name] != undefined) {
-		var operatorModule = exports.specialOperators[operatorTerm.name];
+	} else if (specialOperatorModules[operatorTerm.name] != undefined) {
+		var operatorModule = specialOperatorModules[operatorTerm.name];
 		operatorModule(stack);
 	} else if (basicOperatorModules[operatorTerm.name] != undefined) {
 		var operatorModule = basicOperatorModules[operatorTerm.name];
